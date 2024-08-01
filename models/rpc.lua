4faceb6731e499910e7e71811155804eb0d7a219
Carl Smith <carl.smith@alliedtelesis.co.nz>
2024-08-02 11:12:46 +1200

    More example models to better test actions on lists

--- models/rpc.lua
+++ models/rpc.lua
@@ -59,16 +59,56 @@ local function get_reset_history()
     return { ["last-reset"] = lasttime, ["history"] = history }
 end
 
-local function set_age(input, path)
-    local user = path:match('/t4:test/state/users/([^/]+)')
-    local age = input["age"]
-    apteryx.set("/t4:test/state/users/" .. user .. "/age", age)
+local function createget_users(input, _, method)
+    if method == "GET" then
+        local users = {}
+        for _, info in pairs(apteryx.get_tree('/t4:test/state/users')) do
+            users[#users + 1] = {
+                ["name"] = info["name"],
+                ["age"] = info["age"],
+            }
+        end
+        return {
+            ["users"] = users,
+        }
+    elseif method == "POST" then
+        if input["name"] == nil or input["age"] == nil then
+            return false
+        end
+        apteryx.set_tree ("/t4:test/state/users/" .. input["name"], {
+            ["name"] = input["name"],
+            ["age"] = input["age"],
+        })
+    end
     return true
 end
 
-local function delete_user(input, path)
+local function modifydelete_user(input, path, method)
     local user = path:match('/t4:test/state/users/([^/]+)')
-    apteryx.prune("/t4:test/state/users/" .. user)
+    if user == nil then
+        return false
+    end
+    if method == "PUT" then
+        if input["age"] == nil or apteryx.get("/t4:test/state/users/" .. user .. '/name') ~= user then
+            return false
+        end
+        apteryx.set_tree("/t4:test/state/users/" .. user, {
+            ["age"]  = input["age"],
+        })
+        return true
+    elseif method == "DELETE" then
+        apteryx.prune("/t4:test/state/users/" .. user)
+        return true
+    end
+    return false
+end
+
+local function set_user_age(input, path)
+    local user = path:match('/t4:test/state/users/([^/]+)')
+    if user == nil then
+        return false
+    end
+    apteryx.set("/t4:test/state/users/" .. user .. "/age", input["age"])
     return true
 end
 
@@ -79,6 +119,7 @@ return {
     { path="/t4:test/state/reset", methods={"GET", "POST"}, handler=reset_state },
     { path="/t4:test/state/get-last-reset-time", methods={"GET", "POST"}, handler=get_reset_time },
     { path="/t4:test/state/get-reset-history", methods={"GET", "POST"}, handler=get_reset_history },
-    { path="/t4:test/state/users/*/set-age", methods={"POST"}, handler=set_age },
-    { path="/t4:test/state/users/*", methods={"DELETE"}, handler=delete_user },
+    { path="/t4:test/state/users", methods={"GET", "POST"}, handler=createget_users },
+    { path="/t4:test/state/users/*", methods={"PUT", "DELETE"}, handler=modifydelete_user },
+    { path="/t4:test/state/users/*/set-age", methods={"POST"}, handler=set_user_age },
 }
